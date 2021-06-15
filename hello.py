364985e9f7c9514f1830c456d9b5b309a00cf64c
BernardoBaltazar <36002439+BernardoBaltazar@users.noreply.github.com>
2021-06-15 17:47:28 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,9 +1,10 @@
 # print_odd_numbers.py
 
 def odds(lLimit,uLimit):
+    result=0
     for j in range(lLimit,uLimit+1):
         if(j%2!=0):
-            print(j)
+            result += j
 
 lLimit=int(input("Enter the lower limit for the range:"))
 uLimit=int(input("Enter the upper limit for the range:"))

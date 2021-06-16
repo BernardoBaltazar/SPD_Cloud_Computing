bb04bd25e6ef5617b85fabde8d8c083a8be08f6e
Pedro Santos <56020997+a61207@users.noreply.github.com>
2021-06-16 10:07:50 +0100

    Update hello.py
    change to add function of noah

--- hello.py
+++ hello.py
@@ -1,14 +1,7 @@
 # print_odd_numbers.py
 
-def odds(lLimit,uLimit):
-    result=0
-    for j in range(lLimit,uLimit+1):
-        if(j%2!=0):
-            result += j
-    return result        
+def add(x,y):
+    return x+y        
 
-lLimit=int(input("Enter the lower limit for the range:"))
-uLimit=int(input("Enter the upper limit for the range:"))
-
-print(odds(lLimit,uLimit))
+print(add(3,5))
 

0e3ace81610b7095f18f8ef7a0901a0f0473e52e
Pedro Santos <56020997+a61207@users.noreply.github.com>
2021-06-16 09:49:38 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -5,9 +5,10 @@ def odds(lLimit,uLimit):
     for j in range(lLimit,uLimit+1):
         if(j%2!=0):
             result += j
+    return result        
 
 lLimit=int(input("Enter the lower pipi for the range:"))
 uLimit=int(input("Enter the upper limit for the range:"))
 
-odds(lLimit,uLimit)
+print(odds(lLimit,uLimit))
 
